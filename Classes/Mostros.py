b86825927a09829fd1ff4eeecd944995e47f0849
Johnathan Vitor <79227592+Johnathan42@users.noreply.github.com>
2021-10-25 20:37:46 -0300

    Versão 0.2
    - Correção Commit passado

---
+++ Classes/Mostros.py
@@ -0,0 +1,43 @@
+import pygame
+
+class monstro
+    def __init__(self) -> None:
+        #states monstro
+        level = 3
+        xp = 5
+
+        gold = 0
+
+        Vida = 40
+        MVida = 40
+
+        Mana = 7
+        MMana = 7
+
+        dano_fisico = (self.força * 0.1)
+        dano_magico = 1
+        dano_critico = dano_fisico * 1
+
+        velocidade_ataque = 10
+        
+        vigor = 0.8
+        chance_crit = 0.06
+        chance_drop = 0.01
+
+        força = 1 #dano_fisico
+
+    def update(self) -> None:
+        if self.Vida == 0:
+            self.die()
+    
+    def spawn(self, possition):
+        self.possition = possition
+
+    def ataque(self, dano, alvo):
+        alvo.hp = alvo.hp - dano
+
+    def die(self):
+        #animação
+        #tirar xp
+        self.spawn()
+
